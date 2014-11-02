2190248ea811c6d81b90ba6bfe6f2c005d236817
SanCoder-Q <sancoder.q@gmail.com>
2014-11-02 19:32:56 +0800

    Basic functions implementation.

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,1 +1,18 @@
-//TODO: Please write code in this file.
+//SanCoder 2014-10-31
+//TW homework: Pos_v1
+//Happy halloween!
+
+// alert(document.currentScript.src);
+
+//output
+function printInventory(barcodeList) {
+  //parameter validation
+  Util.Validate.paraNumValidate(arguments, 1);
+  Util.Validate.nullValidate(arguments);
+
+  //not well here!
+  Mall.ShoppingManager.initShoppingCart(barcodeList);
+
+  var inventory = new Inventory(Mall.ShoppingManager.getCurrentShoppingCart());
+  inventory.print();
+}
